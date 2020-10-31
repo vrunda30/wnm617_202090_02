923534a25d897683628ec65c9173ffdcaf3e2646
Vrunda Bhatt <vrunda@vrundas-mbp.attlocal.net>
2020-10-31 14:15:29 -0700

    jason file

---
+++ bhatt.vrunda/data/json_template.js
@@ -0,0 +1,146 @@
+//https://json-jenerator.com
+//https://konbert.com/convert  
+
+
+
+
+
+//User Template
+
+[
+  '{{repeat(10)}}',
+  {
+  id: '{{index(1)}}',
+  name: '{{firstName()}} {{surname()}}',
+    username: function(){
+      return 'user' + this.id;
+  },
+    email: function(){
+       return this.username + '@gimail.com';
+    },
+    password: 'md5(pass)',
+    
+    
+    
+    img: function(tags) {
+      return 'https://via.placeholder.com/400/' +
+        tags.integer(700,900) +
+        '/fff/?text=' +
+        this.username;
+   },
+    date_create: '{{date(new Date(2020, 0, 1), new Date(), "YYYY-MM-dd hh:mm:ss")}}'
+    
+  }
+  
+  
+]
+
+
+
+//Animal Template
+
+[
+  '{{repeat(50)}}',
+  {
+  id: '{{index(1)}}',
+    user_id:'{{integer(1,10)}}',
+  name: '{{company()}}',
+    
+    type: '{{random("dog","horse","cat")}}',
+    breed: function(tags) {
+      var breeds = {
+        "dog":["labrador","pitbull", "dschund", "yorkie"],
+        "horse":["shetland","unicorn", "shetland"],
+        "cat":["calico","ginger","tuxedo","siamese"]
+      };
+      
+       var chosen_type= breeds[this.type];
+       var chosen_index=tags.integer(0,chosen_type.length-1);
+      return chosen_type[chosen_index];
+    },
+    
+       description: '{{lorem(3, "sentences")}}',
+    
+       img: function(tags){
+      return 'https;//via.placeholder.com/400/' + tags.integer(700,999)+ '/fff/?text=' + this.name;
+    }, 
+       date_create:'{{date(new Date(2020, 0, 1), new Date(), "YYYY-MM-dd hh:mm:ss")}}'
+  }
+]
+
+
+
+//Location Template
+
+
+[
+  '{{repeat(250)}}',
+  {
+  id: '{{index(1)}}',
+    user_id:'{{integer(1,50)}}',
+      
+     lat: '{{floating(37.799046,37.709188)}}',
+    lng:'{{floating(-122.514223,-122.371345)}}',
+    
+    
+       description: '{{lorem(3, "sentences")}}',
+    
+       photo: 'https;//via.placeholder.com/400/', 
+       icon: 'https;//via.placeholder.com/100/?text=ICON', 
+   
+       date_create:'{{date(new Date(2020, 0, 1), new Date(), "YYYY-MM-dd hh:mm:ss")}}'
+  }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+[
+  '{{repeat(5, 7)}}',
+  {
+    _id: '{{objectId()}}',
+    index: '{{index()}}',
+    guid: '{{guid()}}',
+    isActive: '{{bool()}}',
+    balance: '{{floating(1000, 4000, 2, "$0,0.00")}}',
+    picture: 'http://placehold.it/32x32',
+    age: '{{integer(20, 40)}}',
+    eyeColor: '{{random("blue", "brown", "green")}}',
+    name: '{{firstName()}} {{surname()}}',
+    gender: '{{gender()}}',
+    company: '{{company().toUpperCase()}}',
+    email: '{{email()}}',
+    phone: '+1 {{phone()}}',
+    address: '{{integer(100, 999)}} {{street()}}, {{city()}}, {{state()}}, {{integer(100, 10000)}}',
+    about: '{{lorem(1, "paragraphs")}}',
+    registered: '{{date(new Date(2014, 0, 1), new Date(), "YYYY-MM-ddThh:mm:ss Z")}}',
+    latitude: '{{floating(-90.000001, 90)}}',
+    longitude: '{{floating(-180.000001, 180)}}',
+    tags: [
+      '{{repeat(7)}}',
+      '{{lorem(1, "words")}}'
+    ],
+    friends: [
+      '{{repeat(3)}}',
+      {
+        id: '{{index()}}',
+        name: '{{firstName()}} {{surname()}}'
+      }
+    ],
+    greeting: function (tags) {
+      return 'Hello, ' + this.name + '! You have ' + tags.integer(1, 10) + ' unread messages.';
+    },
+    favoriteFruit: function (tags) {
+      var fruits = ['apple', 'banana', 'strawberry'];
+      return fruits[tags.integer(0, fruits.length - 1)];
+    }
+  }
+]
---
+++ bhatt.vrunda/data/track_users.sql
@@ -0,0 +1,13 @@
+
+
+INSERT INTO track_users VALUES
+(1,'Reba Ross','user1','user1@gimail.com',md5("pass"),'https://via.placeholder.com/400/763/fff/?text=user1','2020-06-26 08:45:17'),
+(2,'Summers Peck','user2','user2@gimail.com',md5("pass"),'https://via.placeholder.com/400/795/fff/?text=user2','2020-02-22 03:23:43'),
+(3,'Quinn Hess','user3','user3@gimail.com',md5("pass"),'https://via.placeholder.com/400/704/fff/?text=user3','2020-05-08 03:25:58'),
+(4,'Bell Cooper','user4','user4@gimail.com',md5("pass"),'https://via.placeholder.com/400/882/fff/?text=user4','2020-10-15 09:46:26'),
+(5,'Mclaughlin Guy','user5','user5@gimail.com',md5("pass"),'https://via.placeholder.com/400/746/fff/?text=user5','2020-01-21 03:26:59'),
+(6,'Fran Pennington','user6','user6@gimail.com',md5("pass"),'https://via.placeholder.com/400/858/fff/?text=user6','2020-05-11 08:44:30'),
+(7,'Hawkins Le','user7','user7@gimail.com',md5("pass"),'https://via.placeholder.com/400/869/fff/?text=user7','2020-09-16 08:59:01'),
+(8,'Chang Justice','user8','user8@gimail.com',md5("pass"),'https://via.placeholder.com/400/862/fff/?text=user8','2020-01-28 01:50:39'),
+(9,'Suarez Guthrie','user9','user9@gimail.com',md5("pass"),'https://via.placeholder.com/400/803/fff/?text=user9','2020-04-19 11:30:44'),
+(10,'Abbott Sparks','user10','user10@gimail.com',md5("pass"),'https://via.placeholder.com/400/893/fff/?text=user10','2020-07-20 04:36:48');
