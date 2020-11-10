ef9212a2e07d420f57cbff72ae9255c7d7103684
Vrunda Bhatt <vrunda@vrundas-mbp.attlocal.net>
2020-11-10 13:01:50 -0800

    updated database

--- bhatt.vrunda/js/signin.js
+++ bhatt.vrunda/js/signin.js
@@ -58,9 +58,8 @@ const checkUserId = () => {
    if(sessionStorage.userId === undefined) {
       // not logged in
       if(!p.some(o=>window.location.hash===o))
-        // $.mobile.navigate("#signin-page");
-       $.mobile.navigate("#signin-page");
-   } else {
+         $.mobile.navigate("#signin-page");
+} else {
       // logged in
       if(p.some(o=>window.location.hash===o))
          $.mobile.navigate("#recent-page");
