0df13baeeae5475ad5f67c6c36edb0b4feaab575
Vrunda Bhatt <vrunda@Vrundas-MacBook-Pro.local>
2020-11-15 12:27:10 -0800

    Updated File

--- bhatt.vrunda/data/api.php
+++ bhatt.vrunda/data/api.php
@@ -25,7 +25,7 @@ function fetchAll($r) {
 
 
 // connection, prepared statement, parameters
-function makeQuery($c,$ps,$p, $makeResults=true) {
+function makeQuery($c,$ps,$p,$makeResults=true) {
    try {
       if(count($p)) {
          $stmt = $c->prepare($ps);
@@ -34,7 +34,7 @@ function makeQuery($c,$ps,$p, $makeResults=true) {
          $stmt = $c->query($ps);
       }
 
-      $r  = $makeResults ? fetchAll($stmt) : [];
+      $r = $makeResults ? fetchAll($stmt) : [];
 
       return [
          "result"=>$r
@@ -48,68 +48,59 @@ function makeQuery($c,$ps,$p, $makeResults=true) {
 }
 
 
-function makeStatement($data){
+function makeStatement($data) {
+   $c = makeConn();
+   $t = @$data->type;
+   $p = @$data->params;
 
-      $c = makeConn();
-      $t = @$data->type;
-      $p = @$data->params;
+   switch($t) {
 
-
-   switch ($t) {
       case "users_all":
-         return makeQuery($c,"SELECT * FROM track_users",[]);
-         
-
+         return makeQuery($c,"SELECT * FROM `track_users`",[]);
       case "animals_all":
-         return makeQuery($c,"SELECT * FROM track_animals",[]);
-         
-
+         return makeQuery($c,"SELECT * FROM `track_animals`",[]);
       case "locations_all":
-         return makeQuery($c,"SELECT * FROM track_locations",[]);
+         return makeQuery($c,"SELECT * FROM `track_locations`",[]);
+
 
       case "user_by_id":
-         return makeQuery($c,"SELECT * FROM track_user WHERE id = ?",$p);
-
+         return makeQuery($c,"SELECT * FROM `track_users` WHERE `id` = ?",$p);
       case "animal_by_id":
-         return makeQuery($c,"SELECT * FROM track_animals WHERE id = ?",$p);
-
+         return makeQuery($c,"SELECT * FROM `track_animals` WHERE `id` = ?",$p);
       case "location_by_id":
-         return makeQuery($c,"SELECT * FROM track_locations WHERE id = ?",$p);
-         
+         return makeQuery($c,"SELECT * FROM `track_locations` WHERE `id` = ?",$p);
 
-      case "animal_by_user_id":
-         return makeQuery($c,"SELECT * FROM track_animals WHERE user_id = ?",$p);
 
+      case "animals_by_user_id":
+         return makeQuery($c,"SELECT * FROM `track_animals` WHERE `user_id` = ?",$p);
       case "locations_by_animal_id":
-         return makeQuery($c,"SELECT * FROM track_locations WHERE animal_id = ?",$p);
+         return makeQuery($c,"SELECT * FROM `track_locations` WHERE `animal_id` = ?",$p);
 
 
 
       case "check_signin":
-          return makeQuery($c,"SELECT * FROM track_users WHERE username = ? AND password = md5(?)",$p);
+         return makeQuery($c,"SELECT * FROM `track_users` WHERE `username` = ? AND `password` = md5(?)",$p);
 
 
+      case "recent_locations":
+         return makeQuery($c,"SELECT * FROM
+            `track_animals` a
+            LEFT JOIN (
+               SELECT * FROM `track_locations`
+               ORDER BY `date_create` DESC
+            ) l
+            ON a.id = l.animal_id
+            WHERE user_id = ?
+            GROUP BY l.animal_id
+            ",$p);
 
-
-
-
-         default:
-         return ["error"=>"No Matched Type"];
+      default: return ["error"=>"No Matched type"];
    }
-
 }
 
 
 
-
-
-
-
-
-
 $data = json_decode(file_get_contents("php://input"));
-
-
 
 
 echo json_encode(
