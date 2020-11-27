b08344135705ea778d36123b62ca9d091cd9128b
Vrunda Bhatt <vrunda@Vrundas-MacBook-Pro.local>
2020-11-26 16:46:43 -0800

    updated file

--- bhatt.vrunda/js/maps.js
+++ bhatt.vrunda/js/maps.js
@@ -1,11 +1,48 @@
 
 
 const makeMap = (target, center={ lat: 37.767657, lng: -122.484249 }) => {
-   
+   await checkData(()=>window.google);
 	
 	let map_el = $(target);
 
-    
+    if(!map_el.data('map'))
+      map_el.data(
+         "map",
 	    new google.maps.Map(map_el[0], {
         center: center,
         zoom: 12,
+        disableDefaultUI: true
+         })
+      );
+
+   return map_el;
+}
+
+
+
+const makeMarkers = (map_el, map_locs) => {
+
+   let map = map_el.data('map');
+   let markers = map_el.data("markers");
+
+   if(markers) markers.forEach(o=>o.setMap(null));
+
+   markers = [];
+
+   map_locs.forEach(o=>{
+      let m = new google.maps.Marker({
+         position: o,
+         map: map,
+         icon:{
+            url:o.icon,
+            scaledSize: {
+               width:40,
+               height:40
+            }
+         }
+      });
+      markers.push(m);
+   });
+
+   map_el.data("markers",markers);
+}
--- bhatt.vrunda/js/pages.js
+++ bhatt.vrunda/js/pages.js
@@ -1,63 +1,85 @@
 
-const RecentPage= async() => {
-  console.log(google)
-  
-  let d = await query({
-    type: 'recent_location',
-    params:[sessionStorage.userId]
-  });
+const RecentPage = async() => {
 
-  console.log(d)
+   let d = await query({
+      type:'recent_locations',
+      params:[sessionStorage.userId]
+   });
 
-   await checkData(()=>window.google);
+   console.log(d)
 
-   makeMap("#recent-page .map");
-    
-    
+   let valid_animals = d.result.reduce((r,o)=>{
+      o.icon = o.img;
+      if(o.lat && o.lng) r.push(o);
+      return r;
+   },[])
+
+
+   let map_el = await makeMap("#recent-page .map");
+
+   //console.log(map_el.data('map'))
+
+   makeMarkers(map_el,valid_animals);
+
 }
 
-//async and await
+
+
+
+
+// async and await
 const ListPage = async() => {
-  let d = await query({
-    type:'animals_by_user_id',
-    params:[sessionStorage.userId]
-  });
+   let d = await query({
+      type:'animals_by_user_id',
+      params:[sessionStorage.userId]
+   });
 
+   console.log(d)
 
-  console.log(d)
-
-    $("#list-page .animallist")
-    .html(makeAnimalList(d.result));
+   $("#list-page .animallist")
+      .html(makeAnimalList(d.result));
 }
+
+
+
+
 
 
 const UserProfilePage = async() => {
-      let d = await query({
-        type:'user_by_id',
-        params:[sessionStorage.userId]
-      });
+   let d = await query({
+      type:'user_by_id',
+      params:[sessionStorage.userId]
+   });
 
+   console.log(d)
 
-  console.log(d)
-
-    $("#user-profile-page .profile")
-    .html(makeUserProfile(d.result));
+   $("#user-profile-page .profile")
+      .html(makeUserProfile(d.result));
 }
+
+
+
 
 
 
 const AnimalProfilePage = async() => {
-      
-      let d = await query({
-        type:'animal_by_id',
-        params:[sessionStorage.animalId]
-      });
+   query({
+      type:'animal_by_id',
+      params:[sessionStorage.animalId]
+   }).then(d=>{
+      console.log(d)
 
+      $("#animal-profile-page .profile")
+         .html(makeAnimalProfile(d.result));
+   });
 
-  console.log(d)
-
-    $("#animal-profile-page .profile")
-    .html(makeAnimalProfile(d.result));
-
-    makeMap("#animal-profile-page .map")
+   query({
+      type:'locations_by_animal_id',
+      params:[sessionStorage.animalId]
+   }).then(d=>{
+      makeMap("#animal-profile-page .map").then(map_el=>{
+         makeMarkers(map_el,d.result);
+      })
+   })
+   
 }
